a339245c6e5dedc3f8768effb37c3c4daa426042
sakirucar <sakirucar09@hotmail.com>
2024-01-21 18:56:08 +0300

    fix: fix project infrastructure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 
 export default function Home() {
 
-  const [isOpen,setisOpen]= useState()
-  console.log(isOpen)
+  const [isOpen,setisOpen]= useState<boolean>(false)
+
   return (
     <div className="bg-red-500">Hava proje deneme</div>
 
