c93072ca8b4d4f444eb368ab57faabd5641662d3
sakirucar <sakirucar09@hotmail.com>
2024-01-28 03:37:08 +0300

    city search modüle improved and unit switch added

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,32 +1,78 @@
-import React, { useState, Dispatch, SetStateAction } from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import { Icon } from "@iconify/react";
+import {
+  Autocomplete,
+  FormControlLabel,
+  Switch,
+  TextField,
+} from "@mui/material";
 
 interface NavbarProps {
   unit: string;
-  setUnit: Dispatch<SetStateAction<string>>;
+  city: string;
+  setUnit: (value: string) => void;
+  locationData: any;
+  setCity: (value: string) => void;
+  onSearch: () => void;
+  weatherForecastMutate: () => void;
 }
 
 const Navbar = (props: NavbarProps) => {
   const options: string[] = ["Celcius", "Fahrenheit"];
 
+  const handleSearchClick = () => {
+    props.onSearch();
+    props.weatherForecastMutate();
+  };
+
+  interface AutocompleteOption {
+    id: number;
+    name: string;
+    region: string;
+    country: string;
+    lat: number;
+    lon: number;
+    url: string;
+  }
+
+  const locations: AutocompleteOption[] = props.locationData ?? [];
+
   return (
     <div className="bg-gray-500">
       <nav className="flex">
         <div className="flex">
-          <input type="text" />
-          <Icon icon="ic:round-search" />
+          <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={locations}
+            getOptionLabel={(option) => option.name}
+            onChange={(handleSearchClick )}
+
+            onClick={handleSearchClick}
+            sx={{ width: 300 }}
+            renderInput={(params) => (
+              <TextField
+                onChange={(e) => props.setCity(e.target.value)}
+                {...params}
+                label="Cities"
+              />
+            )}
+          />
+          <Icon
+            className="cursor-pointer"
+            onClick={handleSearchClick}
+            icon="ic:round-search"
+          />
         </div>
         <div className="flex justify-end bg-red-500 w-1/3">
-          <select
-            value={props.unit}
-            onChange={(e) => props.setUnit(e.target.value)}
-          >
-            {options.map((option, index) => (
-              <option key={index} value={option}>
-                {option}
-              </option>
-            ))}
-          </select>
+          <Switch
+            checked={props.unit === "Fahrenheit"}
+            onChange={() =>
+              props.setUnit(
+                props.unit === "Fahrenheit" ? "Celcius" : "Fahrenheit"
+              )
+            }
+          />
         </div>
       </nav>
     </div>
