030e62677835f230af4c0ab7688045d6d26a4be0
sakirucar <sakirucar09@hotmail.com>
2024-02-17 19:14:28 +0300

    development feat: commet line added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import { swrDefaultValues } from "@/configs/swr";
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
-import { SWRConfig } from "swr";
+import type { AppProps } from 'next/app'
+import { swrDefaultValues } from '@/configs/swr'
+import { SWRConfig } from 'swr'
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SWRConfig value={swrDefaultValues}>
       <Component {...pageProps} />
     </SWRConfig>
-  );
+  )
 }
