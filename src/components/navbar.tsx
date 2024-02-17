9249f765d59ec261032d81fb13a505c220134b74
sakirucar <sakirucar09@hotmail.com>
2024-02-17 18:42:37 +0300

    adapting-to-responsive-design feat: the project has been made responsive

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,69 +1,66 @@
-import React from "react";
-import { Icon } from "@iconify/react";
-import { Autocomplete, Switch, TextField } from "@mui/material";
+import React from 'react'
+import { Icon } from '@iconify/react'
+import { Autocomplete, Switch, TextField } from '@mui/material'
 
 interface NavbarProps {
-  unit: string;
-  city: string;
-  setUnit: (value: string) => void;
-  locationData: any;
-  setCity: (value: string) => void;
-  onSearch: () => void;
-  weatherForecastMutate: () => void;
+  unit: string
+  city: string
+  setUnit: (value: string) => void
+  locationData: any
+  setCity: (value: string) => void
+  onSearch: () => void
+  weatherForecastMutate: () => void
 }
 
 const Navbar = (props: NavbarProps) => {
   const handleSearchClick = () => {
-    props.onSearch();
-    props.weatherForecastMutate();
-  };
-
-  interface AutocompleteOption {
-    id: number;
-    name: string;
-    region: string;
-    country: string;
-    lat: number;
-    lon: number;
-    url: string;
+    props.onSearch()
+    props.weatherForecastMutate()
   }
 
-  const locations: AutocompleteOption[] = props.locationData ?? [];
+  interface AutocompleteOption {
+    id: number
+    name: string
+    region: string
+    country: string
+    lat: number
+    lon: number
+    url: string
+  }
+
+  const locations: AutocompleteOption[] = props.locationData ?? []
 
   return (
-    <nav className="flex justify-end bg-gradient-to-b from-sky-500 font-lato">
-      <div className="flex items-center justify-end w-1/2 p-3">
+    <nav className='flex justify-end bg-gradient-to-b from-sky-500 font-lato'>
+      <div className='flex items-center justify-end w-1/2 p-3'>
         <Autocomplete
           disablePortal
-          id="combo-box-demo"
+          id='combo-box-demo'
           options={locations}
-          getOptionLabel={(option) => option.name}
+          getOptionLabel={option => option.name}
           onChange={handleSearchClick}
           onClick={handleSearchClick}
           sx={{ width: 300 }}
-          renderInput={(params) => (
-            <TextField className="bg-white rounded-lg"
-              onChange={(e) => props.setCity(e.target.value)}
+          renderInput={params => (
+            <TextField
+              className=' rounded-lg'
+              onChange={e => props.setCity(e.target.value)}
               {...params}
-              label="Search Location"
+              label='Search Location'
             />
           )}
         />
       </div>
-      <div className="flex justify-end items-center w-1/2">
-        <Icon fontSize={70} icon="meteocons:celsius-fill" />
+      <div className='flex justify-end items-center w-1/2'>
+        <Icon fontSize={70} icon='meteocons:celsius-fill' />
         <Switch
-          checked={props.unit === "Fahrenheit"}
-          onChange={() =>
-            props.setUnit(
-              props.unit === "Fahrenheit" ? "Celcius" : "Fahrenheit"
-            )
-          }
+          checked={props.unit === 'Fahrenheit'}
+          onChange={() => props.setUnit(props.unit === 'Fahrenheit' ? 'Celcius' : 'Fahrenheit')}
         />
-        <Icon fontSize={70} icon="meteocons:fahrenheit-fill" />
+        <Icon fontSize={70} icon='meteocons:fahrenheit-fill' />
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
